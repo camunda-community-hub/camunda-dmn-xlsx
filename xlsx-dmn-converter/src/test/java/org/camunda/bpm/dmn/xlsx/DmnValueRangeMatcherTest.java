4f8ab11fe6f0a834bc0af3640acfb03b9cbe4a4a
arolfes <arolfes@users.noreply.github.com>
2023-10-25 19:23:40 +0200

    minimal changes to make it run with camunda 7.20 and switch to jakarta

--- xlsx-dmn-converter/src/test/java/org/camunda/bpm/dmn/xlsx/DmnValueRangeMatcherTest.java
+++ xlsx-dmn-converter/src/test/java/org/camunda/bpm/dmn/xlsx/DmnValueRangeMatcherTest.java
@@ -17,49 +17,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * @author Thorben Lindhauer
  *
  */
-@RunWith(Parameterized.class)
 public class DmnValueRangeMatcherTest {
 
-  @Parameters
-  public static Iterable data() {
-      return Arrays.asList(new Object[][] {
-            { "[1..9]", true },
-            { "]1..9]", true },
-            { "[1..9[", true },
-            { "]1..9[", true },
-            { "[100..900]", true },
-            { "[10.1..909]", true },
-            { "[10..90.1]", true },
-            { "[10.1..90.1]", true },
-            { "text", false },
-            { "[a..b]", false },
-            { "[100..a]", false },
-            { "[100..900", false },
-            { "100..900", false },
-            { "[100900]", false },
-            { "[100.900]", false },
-            { "[date and time(\"2018-05-17T00:00:00\")..date and time(\"2018-11-17T24:00:00\")]", true}
-         });
-     }
-
-  @Parameter(0)
-  public String input;
-
-  @Parameter(1)
-  public boolean shouldMatch;
-
-  @Test
-  public void shouldMatchInclusiveInterval()
+  @ParameterizedTest
+  @CsvSource({
+      "[1..9], true ",
+      "]1..9], true ",
+      "[1..9[, true ",
+      "]1..9[, true ",
+      "[100..900], true ",
+      "[10.1..909], true ",
+      "[10..90.1], true ",
+      "[10.1..90.1], true ",
+      "text, false ",
+      "[a..b], false ",
+      "[100..a], false ",
+      "[100..900, false ",
+      "100..900, false ",
+      "[100900], false ",
+      "[100.900], false ",
+      "[date and time(\"2018-05-17T00:00:00\")..date and time(\"2018-11-17T24:00:00\")], true"
+  })
+  public void shouldMatchInclusiveInterval(String input, boolean shouldMatch)
   {
     Matcher matcher = DmnValueRangeConverter.RANGE_REGEX.matcher(input);
 
